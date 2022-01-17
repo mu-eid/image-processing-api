b6698c47dfbab87b0f9051634aa391f049788680
Muhammad Eid <mu.cs.eid@gmail.com>
2022-01-18 00:12:25 +0200

    Minor refactoring for the resize test file

--- src/test/lib/resize.spec.ts
+++ src/test/lib/resize.spec.ts
@@ -3,18 +3,24 @@ import resize from '../../lib/resize';
 
 describe('resize-lib', () => {
     describe('resize function', () => {
-        it('should produce an output file if an input file exists.', async () => {
-            const name = 'fjord';
-            const width = 300;
-            const height = 200;
+        const name = 'fjord';
+        const width = 300;
+        const height = 200;
+        const outputName = `${name}-w${width}-h${height}.jpg`;
 
+        beforeAll(async () => {
             await resize(name, width, height);
+        });
 
-            const outputName = `${name}-w${width}-h${height}.jpg`;
-
+        it('should produce an output file if an input file exists.', async () => {
             const images = await readdir('./images/out');
-
             expect(images.includes(outputName)).toBe(true);
+        });
+
+        it('should throw an error if input image does not exist.', async () => {
+            await expectAsync(
+                resize('no-file-with-that-name', 300, 200)
+            ).toBeRejected();
         });
     });
 });
