08d7868a0e327f33f9dec08378b3bda33f9425f2
Muhammad Eid <mu.cs.eid@gmail.com>
2022-01-19 16:49:12 +0200

    Refactor test description messages for server

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,7 +1,7 @@
 import httpTest from 'supertest';
 import { app } from '../index';
 
-describe('Server', () => {
+describe('Server status check', () => {
     describe('GET /ping', () => {
         let resp: httpTest.Response;
 
@@ -9,15 +9,15 @@ describe('Server', () => {
             resp = await httpTest(app).get('/ping');
         });
 
-        it('should respond successfully to a request for /ping endpoint.', () => {
+        it('should respond with status code 200', () => {
             expect(resp.statusCode).toEqual(200);
         });
 
-        it('shuold send a JSON response type.', () => {
+        it('shuold send a response with type JSON.', () => {
             expect(resp.get('Content-Type')).toMatch(/json/);
         });
 
-        it('should send a specific JSON response body.', () => {
+        it('should send a specific JSON payload.', () => {
             expect(resp.get('Content-Length')).toEqual('19');
             expect(resp.body).toEqual({ response: 'pong' });
         });
